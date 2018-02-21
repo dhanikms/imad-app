a88e2745277a8d6c966a762718d7dabebc2bb592
dhanikms <msdhanik11@gmail.com>
2018-02-22 02:15:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,17 @@ var button=document.getElementById('counter');
 button.onclick=function(){
     
 
+    var request=new XMLHttpRequest();
     
-    
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+    request.onreadystatechange=function(){
+        if (request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+  request.open('GET','http://msdhanik11.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
