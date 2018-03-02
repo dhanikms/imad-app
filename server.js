342cd7091f7a22b71d8ce3abc74d6a448e33507e
dhanikms <msdhanik11@gmail.com>
2018-03-02 12:14:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ pool.query("SELECT * FROM ARTICLE where title=" +req.params.articleName,function
     
 });
 
-  res.send(createTemplate(articles[articleName]));
+  res.send(createTemplate(articles[articlename]));
 });
 app.get('/article-two',function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
